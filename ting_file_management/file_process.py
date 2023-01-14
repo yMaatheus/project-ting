34404db481ad0ebc03e18c0f75b1a10c821747fe
Matheus <stvgamer74@gmail.com>
2023-01-14 15:10:31 -0300

    feat: :sparkles: implements in file_process the function process and enqueue to queue instance

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    for item in instance._data:
+        return item["nome_do_arquivo"] == path_file
+
+    text = txt_importer(path_file)
+    queue_item = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text),
+        "linhas_do_arquivo": text,
+    }
+
+    print(queue_item)
+    instance.enqueue(queue_item)
 
 
 def remove(instance):
