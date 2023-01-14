91f6d3bc1fefc7a6b664e6476348b77bc15faeff
Matheus <stvgamer74@gmail.com>
2023-01-14 14:48:03 -0300

    feat: :sparkles: implements the function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if (not path_file.endswith('.txt')):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file) as file:
+            text = file.read().split("\n")
+            return text
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
