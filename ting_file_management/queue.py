1e678a74b3a5a1077f29e64b86b1b98727cdcec2
Matheus <stvgamer74@gmail.com>
2023-01-14 14:37:45 -0300

    feat: :sparkles: implements Queue class methods

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,22 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if (self.isEmpty()):
+            return None
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (len(self._data) <= index or index < 0):
+            raise IndexError
+        return self._data[index]
+
+    def isEmpty(self):
+        return not self._data
