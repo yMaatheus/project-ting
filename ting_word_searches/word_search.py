1a22e5da978468a11dbbd62a80a48819f8eed23b
Matheus <stvgamer74@gmail.com>
2023-01-14 22:04:50 -0300

    feat: :sparkles: implements the function exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,22 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
+def exists_word(word, instance: Queue):
+    result = []
+
+    for item in instance._data:
+        lines = []
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            if word.upper() in line.upper():
+                lines.append({"linha": index + 1})
+        if (len(lines) > 0):
+            result.append({
+                    'palavra': word,
+                    "arquivo": item["nome_do_arquivo"],
+                    "ocorrencias": lines
+                })
+    return result
+
+
+def search_by_word(word, instance: Queue):
     """Aqui irá sua implementação"""
